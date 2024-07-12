81fca073d31416169a9f0deaf159427d1adbf426
makufff <makufff.tanapat@gmail.com>
2024-07-12 11:12:11 +0700

    Week 2

--- Week 2/Donut.py
+++ Week 2/Donut.py
@@ -1,13 +1,19 @@
 """Donut"""
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-promotion_sets = (d - 1) // (b + c) + 1 if b + c > 0 else d
-total_donuts = promotion_sets * (b + c)
-total_cost = promotion_sets * b * a
-if total_donuts < d:
-    extra_donuts = d - total_donuts
-    total_cost += extra_donuts * a
-    total_donuts = d
-print(f"{total_cost} {total_donuts}")
+price = int(input())
+amount = int(input())
+free = int(input())
+want = int(input())
+in_want = want // (amount + free)
+# base case
+if not want:
+    print("0 0")
+elif want > 0:
+    donut = in_want*(amount + free)
+    t_l = want - donut
+    if t_l > amount:
+        t_l = amount
+    if t_l >= amount:
+        donut = donut + (amount + free)
+    else:
+        donut = donut + t_l
+    print(f"{(price*in_want*amount) + (t_l*price)} {donut}")
--- Week 2/Largest Number.py
+++ Week 2/Largest Number.py
@@ -1,23 +1,14 @@
 """CMP"""
-num1 = input()
-num2 = input()
-num3 = input()
-def cmp(a, b):
+def cmp(val_1, val_2):
     """cmp"""
-    return a + b > b + a
-if cmp(num1, num2) and cmp(num1, num3):
-    if cmp(num2, num3):
-        result = num1 + num2 + num3
-    else:
-        result = num1 + num3 + num2
-elif cmp(num2, num1) and cmp(num2, num3):
-    if cmp(num1, num3):
-        result = num2 + num1 + num3
-    else:
-        result = num2 + num3 + num1
-else:
-    if cmp(num1, num2):
-        result = num3 + num1 + num2
-    else:
-        result = num3 + num2 + num1
-print(result)
+    if val_1 > val_2:
+        return val_1
+    return val_2
+a,b,c = input(),input(),input()
+curr = a + b + c
+curr = cmp((a + c + b), curr)
+curr = cmp((b + a + c), curr)
+curr = cmp((b + c + a), curr)
+curr = cmp((c + b + a), curr)
+curr = cmp((c + a + b), curr)
+print(int(curr))
